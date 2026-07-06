cd7f6576a30660af2a5c6575ae7f5a6d43a8cdab
yanwarpro <yanwar.pro@gmail.com>
2026-07-06 09:07:31 +0700

    Update BASE_URL to Cloud Run

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,5 +1,4 @@
-// TODO: Silakan sesuaikan BASE URL dari endpoint Anda
-const BASE_URL = 'YOUR_BASE_URL';
+const BASE_URL = 'https://asclepius-api-678856957398.asia-southeast2.run.app';
 
 const ENDPOINT = {
   predict: `${BASE_URL}/predict`,
